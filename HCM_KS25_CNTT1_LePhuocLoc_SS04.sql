7edd4a191f3e3d2241bbfd5d73b4b96310bb9a4c
Le Phuoc Loc <lephuocloc00z12@gmail.com>
2026-05-06 12:48:43 +0700

    sua bai

--- HCM_KS25_CNTT1_LePhuocLoc_SS04.sql
+++ HCM_KS25_CNTT1_LePhuocLoc_SS04.sql
@@ -1,51 +1,118 @@
--- CREATE DATABASE ShopManager;
+      
+CREATE DATABASE OnlineLearningSystem_db ;
+USE OnlineLearningSystem_db ; 
 
--- USE ShopManager;
+CREATE TABLE Teacher (
+    TeacherID INT PRIMARY KEY AUTO_INCREMENT,
+    FullName NVARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL
+) ENGINE = InnoDB;
 
-CREATE TABLE Categories (
-	category_id INT PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(100) NOT NULL
-);
+CREATE TABLE Course (
+	CourseID INT PRIMARY KEY AUTO_INCREMENT , 
+    CourseName NVARCHAR(200) NOT NULL,
+    Description TEXT,
+    Lessons INT DEFAULT 10,
+    
+    TeacherID INT ,
+    FOREIGN KEY (TeacherID) REFERENCES Teacher(TeacherID) 
+    ON DELETE SET NULL ON UPDATE CASCADE 
+) ENGINE = InnoDB ; 
+ 
+ CREATE TABLE Student (
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    FullName VARCHAR(100) NOT NULL,
+    BirthDate DATE,
+    Email VARCHAR(100) UNIQUE NOT NULL
+) ENGINE=InnoDB;
 
-CREATE TABLE Products(
-	product_id INT PRIMARY KEY AUTO_INCREMENT,
-    product_name VARCHAR(100) NOT NULL,
-    price DECIMAL(18,2) NOT NULL CHECK(price > 0),
-    stock INT NOT NULL CHECK(stock > 0),
-	category_id INT NOT NULL,
-    FOREIGN KEY(category_id) REFERENCES Categories(category_id)
-);
--- thêm dữ liệu cho CateGories
-INSERT INTO Categories(category_name)
+ CREATE TABLE Enrollment (
+	EnrollmentID INT PRIMARY KEY AUTO_INCREMENT ,
+    StudentID INT NOT NULL,
+    CourseID INT NOT NULL,
+    EnrollDate DATE DEFAULT (CURRENT_DATE) ,
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID) ON DELETE CASCADE ,
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID) ON DELETE CASCADE ,
+    
+    UNIQUE KEY Unique_Enrollment (StudentID, CourseID)
+ ) ENGINE = InnoDB; 
+ 
+ CREATE TABLE Score (
+	ScoreID INT PRIMARY KEY AUTO_INCREMENT, 
+    StudentID INT NOT NULL ,
+    CourseID INT NOT NULL ,
+    MidtermScore DECIMAL(4,2) ,
+    FinalScore DECIMAL(4,2) ,
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID) ON DELETE CASCADE ,
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID) ON DELETE CASCADE ,
+    
+    CONSTRAINT Check_Score_Range CHECK (MidtermScore BETWEEN 0 AND 10 AND FinalScore BETWEEN 0 AND 10 ) ,
+    UNIQUE KEY Unique_Result (StudentID , CourseID) 
+ ) ENGINE = InnoDB ; 
+ 
+ 
+ INSERT INTO Teacher (FullName , Email) 
+ VALUES 
+('Nguyễn Văn A', 'anv@university.edu.vn'),
+('Trần Thị B', 'btt@university.edu.vn'),
+('Lê Văn C', 'clv@university.edu.vn'),
+('Phạm Minh D', 'dpm@university.edu.vn'),
+('Hoàng Lan E', 'ehl@university.edu.vn'); 
+
+INSERT INTO Course (CourseName , Description , Lessons, TeacherID) 
 VALUES 
-('Điện tử'),
-('Thời trang');
+('Lập trình C cơ bản', 'Học về kiểu dữ liệu, vòng lặp', 15, 1),
+('Cơ sở dữ liệu SQL', 'Thiết kế và truy vấn CSDL', 12, 2),
+('Web Frontend', 'HTML, CSS và JavaScript', 20, 3),
+('Cấu trúc dữ liệu', 'Giải thuật và tổ chức dữ liệu', 18, 1),
+('Mạng máy tính', 'Giao thức và mô hình OSI', 10, 4); 
 
--- Thêm dữ liệu cho Products
-INSERT INTO Products(product_name, price, stock , category_id)
-VALUES
-('iPhone 15', 25000000 , 10, 1),
-('Samsung S23', 20000000 , 5, 1),
-('Áo sơ mi nam', 500000, 50, 2),
-('Giày thể thao', 1200000, 20, 2);
+INSERT INTO Student (FullName ,BirthDate,Email) 
+VALUES 
+('Nguyễn Văn Nam', '2005-05-15', 'namnv@gmail.com'),
+('Lê Thị Hoa', '2005-08-20', 'hoalt@gmail.com'),
+('Trần Minh Tuấn', '2004-12-01', 'tuanmt@gmail.com'),
+('Phạm Thảo Vy', '2005-02-10', 'vypt@gmail.com'),
+('Đỗ Hùng Dũng', '2005-11-30', 'dungdh@gmail.com'); 
 
--- CẬP NHẬT DỮ LIỆU
-SET SQL_SAFE_UPDATES = 0;
-UPDATE Products
-SET price = 26000000 WHERE product_name = 'iPhone 15';
+INSERT INTO Enrollment (StudentID, CourseID, EnrollDate)
+VALUES 
+(1, 1, '2026-04-01'),
+(1, 2, '2026-04-02'),
+(2, 2, '2026-04-01'),
+(3, 3, '2026-04-05'),
+(4, 1, '2026-04-10'); 
 
-UPDATE Products
-SET stock = stock + 10
-WHERE category_id = 1;
+USE OnlineLearningsystem_db ;
+INSERT INTO Score (StudentID, CourseID, MidtermScore) VALUES 
+(5, 2, 9.5) ; 
+ 
 
--- XÓA DỮ LIỆU
-DELETE FROM Products
-WHERE product_id = 4 OR price < 1000000;
+UPDATE Student
+SET Email = 'nam_new_2026@gmail.com' 
+WHERE StudentID = 1;
+
+UPDATE Course 
+SET Description = 'Học MySQL cơ bản và nâng cao'
+ WHERE CourseID = 2;
+ 
+UPDATE Score 
+SET FinalScore = 9.5 
+WHERE StudentID = 1 AND CourseID = 1;
+
+DELETE FROM Enrollment 
+WHERE EnrollmentID = 5 ; 
+
+SELECT * FROM Student; 
+
+SELECT c.CourseName, t.FullName AS Instructor
+FROM Course c
+LEFT JOIN Teacher t ON c.TeacherID = t.TeacherID; 
+
+SELECT s.FullName, c.CourseName, sc.MidtermScore, sc.FinalScore
+FROM Score sc
+JOIN Student s ON sc.StudentID = s.StudentID
+JOIN Course c ON sc.CourseID = c.CourseID; 
 
 
--- TRUY VẤN
-SELECT * FROM Product;
-SELECT * FROM Products WHERE stock > 15;
-SELECT * FROM Products WHERE Price >= 1000000 AND Price <= 25000000;
-SELECT * FROM Products WHERE product_name <> 'iPhone 15' AND stock > 0;
-SELECT * FROM Products WHERE category_id = 1 AND price > 500000;
+    
